4ccaee09be11e86818262b3dcf45f62d3d5419e6
Sampriti Mukherjee <sampriti275@gmail.com>
2023-04-26 02:47:53 +0530

    Home page completed

--- README.md
+++ README.md
@@ -1,70 +1,40 @@
-# Getting Started with Create React App
+# React Admin Dashboard
+### build in progress...
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Gmail Clone is a web application that provides a user interface similar to that of Gmail. The application uses Google authentication to authorize users, and once authorized, users can view their emails in their inbox, compose and send emails, and view their sent emails in their inbox.
 
-## Available Scripts
+## Features
 
-In the project directory, you can run:
+- Customizable dashboard theme and design.
+- Interactive charts, graphs, and tables.
 
-### `npm start`
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Technology Used
 
-### `npm test`
+- Reactjs for frontend.
+- Tailwind CSS for styling. 
+- Context API for state management.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `npm run build`
+## Home Page
+![Home Page](https://res.cloudinary.com/dewu8pifs/image/upload/v1682457191/dashboardHome_us95vg.png)
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+![Revenue section](https://res.cloudinary.com/dewu8pifs/image/upload/v1682457191/revenue_lqy7aq.png)
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+## Installation
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+To install and run this project locally, follow these steps:
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+1. Clone this repository using `git clone https://github.com/sampriti27/Admin-Dashboard`
+2. Navigate to the project directory with `cd Admin-Dashboard`
+3. Install dependencies with `npm install`
+4. Start the development server with `npm start`
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
 
-## Learn More
+## Contact
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+If you have any questions or feedback, please contact me at sampriti275@gmail.com I'd love to hear from you!
---
+++ src/pages/Ecommerce.jsx
@@ -0,0 +1,137 @@
+import React from "react";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
+
+import { Stacked, Pie, Button, SparkLine } from "../components";
+import {
+  earningData,
+  sparkLineData,
+  ecomPieChartData,
+  SparklineAreaData,
+} from "../data/dummy";
+
+import { useStateContext } from "../contexts/Contextrovider";
+const Ecommerce = () => {
+  return (
+    <div className="mt-12">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <p className="font-bold text-blue-800 text-lg">Earnings</p>
+              <p className="text-2xl">&#8377;63,448.78</p>
+            </div>
+          </div>
+          <div className="mt-6">
+            <Button
+              color="white"
+              bgColor="blue"
+              text="Download"
+              borderRadius="10px"
+              size="md"
+            />
+          </div>
+        </div>
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
+            >
+              <button
+                type="button"
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className="text-2xl opacity-0.9 p-4 rounded-full hover:drop-shadow-xl"
+              >
+                {item.icon}
+              </button>
+              <p className="mt-3">
+                <span className="text-lg font-semibold"> {item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className="text-sm mt-1 text-gray-400">{item.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Revenue System  */}
+
+      <div className="flex gap-18 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+          <div className="flex justify-between ">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+
+              <p
+                className="flex items-center gap-2 hover:drop-shadow-xl"
+                style={{ color: "#25C4B8" }}
+              >
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="text-3xl p-1 font-semibold">
+                    &#8377;93,438
+                  </span>
+                  <span className="p-1.5 hover:drop-shadow-xl text-white cursor-pointer rounded-full bg-green-400 ml-3 text-xs">
+                    23%
+                  </span>
+                </p>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+              <div className="mt-8">
+                <p>
+                  <span className="text-3xl p-1 font-semibold">
+                    &#8377;48,438
+                  </span>
+                </p>
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+
+              {/* SparkLine Chart  */}
+              <div className="mt-5">
+                <SparkLine
+                  currentColor="blue"
+                  id="line-sparkline"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                />
+              </div>
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+            <div>
+              <Stacked width="320px" height="360px" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ecommerce;
